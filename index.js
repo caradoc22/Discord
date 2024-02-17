46452430788e482207622a36eddf3c9e04381c94
caradoc22 <158219951+caradoc22@users.noreply.github.com>
2024-02-17 14:01:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ keepAlive();
 function formatTime() { //Credits to himika#0001 and never#0001
   const date = new Date();
   const options = {
-    timeZone: 'America/Port_of_Spain', //https://www.zeitverschiebung.net/en/ and find your city and enter here
+    timeZone: 'Vietnam/Ho_Chi_Minh_City', //https://www.zeitverschiebung.net/en/ and find your city and enter here
     hour12: true,
     hour: 'numeric',
     minute: 'numeric'
@@ -23,28 +23,28 @@ client.on('ready', async () => {
   console.log(`${client.user.tag} - rpc started! check your profile ^^`);
 
   const r = new Discord.RichPresence()
-.setApplicationId('1189883886579232788')
+.setApplicationId('1046430425905643612')
     .setType('STREAMING')
-    .setURL('https://www.youtube.com/watch?v=4-siHP8YjhI') //Must be a youtube video link 
-    .setState('Sleepy')
-    .setName('randomness')
-    .setDetails(`Hey 👋⚓ [${formatTime()}]`)
+    .setURL('https://www.youtube.com/watch?v=A2cu_za6-bI&t=535s') //Must be a youtube video link 
+    .setState('Suy vai o')
+    .setName('hmm')
+    .setDetails(`24/7 Tương Tư Về Em [${formatTime()}]`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1150242860273254460/1201348112325824592/latest_banner.gif') //You can put links in tenor or discord and etc.
+ .setAssetsLargeImage('https://tenor.com/vi/view/my-dress-up-darling-anime-love-marin-kitagawa-gif-25055013') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('insert funny text') //Text when you hover the Large image
-    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1150242860273254460/1190250393746280509/cc5323ff-e634-4a9b-8320-2cb09efa665a.gif') //You can put links in tenor or discord and etc.
+    .setAssetsSmallImage('https://tenor.com/vi/view/pink-verify-check-gif-23942665') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('You weirdo, why would you hover over the small image.') //Text when you hover the Small image
-    .addButton('My Site 🔨', 'https://vornexx.is-a.dev')
-    .addButton('My Rentry Page ✏️💞', 'https://rentry.co/vornexx')
+    .addButton('Discord 🔨', 'https://discord.gg/DhT7adeM')
+    .addButton('Youtube ✏️💞', 'https://www.youtube.com/watch?v=Pi_mL1ONhGk')
 
   client.user.setActivity(r);
-  client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
+  client.user.setPresence({ status: "idle" }); //dnd, online, idle, offline
 
   let prevTime = null;
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = `Hey 👋⚓ [${newTime}]`;
+      const newDetails = `24/7 Tương Tư Về Em [${newTime}]`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
